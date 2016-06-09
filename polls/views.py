eb0e4d30bd5b493205e1dc91e7a3849424ef7429
Steve Mbiele <mbielesteve@hotmail.com>
2016-06-09 13:53:36 -0400

    part 4 done

--- polls/views.py
+++ polls/views.py
@@ -1,24 +1,50 @@
-from django.http import Http404
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views import generic
+from django.db.models import F
 
-from .models import Question
+from .models import Choice, Question
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def detail(request, question_id):
-    return HttpResponse("You are looking at question %s." % question_id)
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
-def results(request, question_id):
-    response = "You are looking at the results of the question %s."
-    return HttpResponse(response % question_id)
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
-    return HttpResponse("you are voting on question %s." % question_id)
-
-def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+#       redisplay the question with an error message notifying the user that he did not choose an answer
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice."
+        })
+    else:
+        #selected_choice.votes += 1
+        ########################################################################################################################
+        # If 2 people are saving their votes at the same time, the version upward might cause problems because it is the python
+        # BACKEND that is in charge of adding that field, and since we are working in a multithreaded inviroment, the results
+        # might notbe what we intend it to be. To fix that, we can use the F() function when adding they vote value, that
+        # will overwrite the python code and ask the database to update its values directly
+        ########################################################################################################################
+        selected_choice.votes = F('votes') + 1
+        selected_choice.save()
+        # Note: always return an HttpResponseRedirect after a susccesful post, to prevent data from
+        # being posted twice if the user press on the back button
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
